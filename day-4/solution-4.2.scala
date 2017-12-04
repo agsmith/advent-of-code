3d53b4aeac0805d4ee309cf0a32a42a74f51e1e2
asmith <asmith@ccadllc.us>
2017-12-04 12:48:38 -0500

    fixup

--- day-4/solution-4.2.scala
+++ day-4/solution-4.2.scala
@@ -1,12 +1,16 @@
 import scala.io.Source
 
-val filename = "/Users/adamsmith/work/ccad/Development/github/advent-of-code/day-4/in.txt"
+val filename = "in.txt"
 
 val lineStrings = Source.fromFile(filename).getLines.toList
 
 val valid = lineStrings.filter { lineString =>
   val strings: List[String] = lineString.split(" ").toList
-  val stringSet = strings.toSet
+  val sortedStrings = strings.map { s =>
+    val deconstructed = s.split("").toList.sortWith(_>_)
+    deconstructed.foldLeft("")(_++_)
+  }
+  val stringSet = sortedStrings.toSet
   stringSet.size == strings.size
 }
 
